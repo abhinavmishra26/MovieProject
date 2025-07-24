45190f4d83c9af3a6e6763f74fc36ce9e5e82bc8
abhinavmishra26 <cseworld73@gmail.com>
2025-07-24 15:47:18 +0530

    import issue fix

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
-import { inngest, functions } from "./src/inngest/index.js"
+import { inngest, functions } from "./inngest/index.js"
 
 
 const port=3000
